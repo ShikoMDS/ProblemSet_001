c6d47e19629e794ca886fde2b7b1faacc5737715
Ayoub <141291923+ShikoMDS@users.noreply.github.com>
2023-08-04 06:21:05 +1200

    Delete weapon.cpp

--- Problem_001_1/weapon.cpp
+++
@@ -1,9 +0,0 @@
-#include "weapon.h"
-#include <iostream>
-
-Weapon::Weapon(const std::string& Name, int Damage) : Name(Name), Damage(Damage) {}
-
-void Weapon::display() const
-{
-	std::cout << "Name: " << Name << "\nDamage: " << Damage << std::endl;
-}
